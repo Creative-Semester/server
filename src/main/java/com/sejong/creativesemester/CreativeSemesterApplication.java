7128f0c67457dc7c8bc184d33225948d54f3e54b
jaepyo99 <jaepyo99@naver.com>
2023-08-14 18:35:12 +0900

    init

---
+++ src/main/java/com/sejong/creativesemester/CreativeSemesterApplication.java
@@ -0,0 +1,13 @@
+package com.sejong.creativesemester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativeSemesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativeSemesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sejong/creativesemester/CreativeSemesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejong.creativesemester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativeSemesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
