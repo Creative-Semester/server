bcad37c01c8d98beb0243845a8ef6c6f00631297
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2023-08-20 22:01:48 +0900

    update userRepository

--- src/main/java/com/sejong/creativesemester/domain/user/repository/UserRepository.java
+++ src/main/java/com/sejong/creativesemester/domain/user/repository/UserRepository.java
@@ -1,2 +1,15 @@
-package com.sejong.creativesemester.domain.user.repository;public class UserRepository {
+package com.sejong.creativesemester.domain.user.repository;
+
+import com.sejong.creativesemester.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+
+    Optional<User> findById(Long userId);
+
+    Optional<User> findByUsername(String username);
+
 }
