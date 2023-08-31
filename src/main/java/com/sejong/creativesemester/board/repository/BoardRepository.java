276f5090811f42302192839d3609d45d82c4d27d
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2023-08-31 18:04:14 +0900

    fix: FreeBoard 삭제 및 Repository 수정

--- src/main/java/com/sejong/creativesemester/board/repository/BoardRepository.java
+++ src/main/java/com/sejong/creativesemester/board/repository/BoardRepository.java
@@ -6,13 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-<<<<<<< HEAD:src/main/java/com/sejong/creativesemester/board/repository/BoardRepository.java
 public interface BoardRepository extends JpaRepository<Board, Long> {
-=======
-import java.util.List;
-
-public interface FreeBoardRepository extends JpaRepository<FreeBoard, Long> {
->>>>>>> jg:src/main/java/com/sejong/creativesemester/freeboard/repository/FreeBoardRepository.java
 
     @Query("select f from FREEBOARD_TABLE as f join f.major as m order by f.createdTime desc")
     Page<Board> findAllByOrderByCreatedDateDesc(long majorId, Pageable pageable);
