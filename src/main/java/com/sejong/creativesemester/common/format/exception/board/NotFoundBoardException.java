06aba4e7482de02ccc8817bc6e1e3d4027922aa1
jaepyo99 <jaepyo99@naver.com>
2023-09-04 02:59:27 +0900

    [Add] 예외처리 추가
    - 동일한 사용자가 아닐때, 학생회가 아닐때의 예외반환 추가

--- src/main/java/com/sejong/creativesemester/common/format/exception/board/NotFoundBoardException.java
+++ src/main/java/com/sejong/creativesemester/common/format/exception/board/NotFoundBoardException.java
@@ -3,10 +3,10 @@ package com.sejong.creativesemester.common.format.exception.board;
 import com.sejong.creativesemester.common.format.exception.ApplicationRunException;
 import com.sejong.creativesemester.common.format.exception.ErrorEnumCode;
 
-import static com.sejong.creativesemester.common.format.exception.board.errorCode.BoardErrorCode.Not_Found_BOARD;
+import static com.sejong.creativesemester.common.format.exception.board.errorCode.BoardErrorCode.NOT_FOUND_BOARD;
 
 public class NotFoundBoardException extends ApplicationRunException {
-    private static final ErrorEnumCode CODE = Not_Found_BOARD;
+    private static final ErrorEnumCode CODE = NOT_FOUND_BOARD;
     private NotFoundBoardException(ErrorEnumCode CODE){
         super(CODE);
     }
