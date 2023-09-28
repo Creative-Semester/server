f5bc08d30a577c52e23de662dcd8c0c4682f77ea
jaepyo99 <jaepyo99@naver.com>
2023-09-28 19:31:56 +0900

    [Docs] 스웨거 파라미터 설명 추가

--- src/main/java/com/sejong/creativesemester/comment/controller/req/AddCommentRequest.java
+++ src/main/java/com/sejong/creativesemester/comment/controller/req/AddCommentRequest.java
@@ -4,9 +4,13 @@ import com.sejong.creativesemester.comment.service.req.AddCommentRequestDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Schema
 @Getter
 public class AddCommentRequest {
+
+    @NotBlank(message = "댓글 내용을 입력해주세요")
     private String text;
 
     public AddCommentRequestDto toRequestDto() {
