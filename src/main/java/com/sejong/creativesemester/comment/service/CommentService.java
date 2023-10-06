2c928c74c7ab74c5a9ff9b5e32261ab26b5ab335
jaepyo99 <jaepyo99@naver.com>
2023-10-06 17:53:02 +0900

    [Feat] 댓글 조회시 페이지네이션
    - 댓글 조회할때 페이징처리 추가

--- src/main/java/com/sejong/creativesemester/comment/service/CommentService.java
+++ src/main/java/com/sejong/creativesemester/comment/service/CommentService.java
@@ -2,24 +2,25 @@ package com.sejong.creativesemester.comment.service;
 
 import com.sejong.creativesemester.board.entity.Board;
 import com.sejong.creativesemester.board.repository.BoardRepository;
-import com.sejong.creativesemester.comment.controller.res.CommentListResponse;
 import com.sejong.creativesemester.comment.entity.Comment;
 import com.sejong.creativesemester.comment.repository.CommentRepository;
 import com.sejong.creativesemester.comment.repository.CommentRepositoryCustom;
 import com.sejong.creativesemester.comment.repository.dto.CommentListDto;
 import com.sejong.creativesemester.comment.service.req.AddCommentRequestDto;
-import com.sejong.creativesemester.comment.service.res.CommentListResponseDto;
+import com.sejong.creativesemester.comment.service.res.CommentInfoDto;
 import com.sejong.creativesemester.common.format.exception.board.NotFoundBoardException;
 import com.sejong.creativesemester.common.format.exception.user.NotFoundUserException;
 import com.sejong.creativesemester.common.meta.DistributeLock;
 import com.sejong.creativesemester.user.entity.User;
 import com.sejong.creativesemester.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.security.Principal;
 import java.util.List;
 
 import static java.lang.Boolean.FALSE;
@@ -34,6 +35,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentRepositoryCustom commentRepositoryCustom;
     private static final String COMMENT_KEY = "COMMENT_";
+    private final static int TOTAL_CONTENT_PER_PAGE = 20;
 
     public void addComment(Long boardId, String studentNum, AddCommentRequestDto addCommentRequestDto) {
         Board boardById = boardRepository.findById(boardId).orElseThrow(NotFoundBoardException::new);
@@ -55,29 +57,34 @@ public class CommentService {
     }
 
     @DistributeLock(identifier = COMMENT_KEY, key = "#commentId")
-    public void reportComment(String studentNum,Long commentId) {
+    public void reportComment(String studentNum, Long commentId) {
         Comment commentById = commentRepository.findById(commentId).orElseThrow(
                 () -> new NullPointerException("존재하지 않는 댓글입니다.")
         );
         commentById.reportComment();
     }
 
-    public List<CommentListResponseDto> getCommentList(String studentNum, Long boardId) {
-        List<CommentListDto> allCommentOfBoard = commentRepositoryCustom.findAllCommentOfBoard(boardId);
-        List<CommentListResponseDto> commentListResponseDtoList = new ArrayList<>();
+    public CommentInfoResponseDto getCommentList(String studentNum, Long boardId, int page) {
+        Page<CommentListDto> allCommentOfBoard = commentRepositoryCustom.findAllCommentOfBoard(
+                boardId, PageRequest.of(page, TOTAL_CONTENT_PER_PAGE));
+        List<CommentInfoDto> commentInfoDto = new ArrayList<>();
         for (CommentListDto commentListDto : allCommentOfBoard) {
             Boolean isMine = FALSE;
-            if(commentListDto.getStudentNum().equals(studentNum)){
+            if (commentListDto.getStudentNum().equals(studentNum)) {
                 isMine = TRUE;
             }
-            commentListResponseDtoList.add(CommentListResponseDto.builder()
+            commentInfoDto.add(CommentInfoDto.builder()
                     .id(commentListDto.getId())
                     .text(commentListDto.getText())
                     .createdTime(commentListDto.getCreatedTime())
                     .isMine(isMine)
                     .build());
         }
-        return commentListResponseDtoList;
+        return CommentInfoResponseDto.builder()
+                .commentInfoDtoList(commentInfoDto)
+                .totalPage(allCommentOfBoard.getTotalPages())
+                .currentPage(allCommentOfBoard.getNumber())
+                .build();
     }
 
 }
