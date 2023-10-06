9094b0878a5bf0dd6daa74cb8ce457d124ec02c2
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2023-10-06 22:47:56 +0900

    Feat: 교수 리스트 조회 기능 추가

--- src/main/java/com/sejong/creativesemester/professor/controller/ProfessorBoardController.java
+++ src/main/java/com/sejong/creativesemester/professor/controller/ProfessorBoardController.java
@@ -1,6 +1,7 @@
 package com.sejong.creativesemester.professor.controller;
 
 import com.sejong.creativesemester.common.format.success.SuccessResponse;
+import com.sejong.creativesemester.professor.service.ProfessorBoardService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,13 @@ import java.security.Principal;
 @RequestMapping("api/v1/professor")
 public class ProfessorBoardController {
 
+    private final ProfessorBoardService professorBoardService;
+
     // 교수 조회, 강의 게시판 조회, 학생들 평가 생성, 조회, 삭제, 신고(?)
     @GetMapping()
     public SuccessResponse getProfessorBoards(@ApiIgnore Principal principal,
                                               @RequestParam(required = false, defaultValue = "0", value = "page") int page){
-
+        return new SuccessResponse(professorBoardService.getBoards(principal.getName(), page));
     }
 
 }
--- src/main/java/com/sejong/creativesemester/professor/service/ProfessorBoardService.java
+++ src/main/java/com/sejong/creativesemester/professor/service/ProfessorBoardService.java
@@ -3,14 +3,18 @@ package com.sejong.creativesemester.professor.service;
 import com.sejong.creativesemester.common.format.exception.user.NotFoundUserException;
 import com.sejong.creativesemester.professor.dto.ProfessorListResponseDto;
 import com.sejong.creativesemester.professor.dto.ProfessorSimpleResponseDto;
+import com.sejong.creativesemester.professor.entity.Professor;
 import com.sejong.creativesemester.professor.repository.ProfessorRepository;
 import com.sejong.creativesemester.user.entity.User;
 import com.sejong.creativesemester.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -19,9 +23,22 @@ public class ProfessorBoardService {
 
     private final UserRepository userRepository;
     private final ProfessorRepository professorRepository;
+    private final int TOTAL_ITEMS_PER_PAGE = 20;
 
     public ProfessorListResponseDto getBoards(String studentNum, int page){
         User user = userRepository.findByStudentNum(studentNum).orElseThrow(NotFoundUserException::new);
-        Page<ProfessorSimpleResponseDto> list = professorRepository.
+        Page<Professor> list = professorRepository.findAllByOrderByName(user.getMajor().getId(), PageRequest.of(page, TOTAL_ITEMS_PER_PAGE));
+
+        return ProfessorListResponseDto.builder()
+                .totalPage(list.getTotalPages())
+                .currentPage(list.getNumber())
+                .list(list.getContent().stream().map((professor -> ProfessorSimpleResponseDto.builder()
+                        .boardId(professor.getId())
+                        .name(professor.getName())
+                        .intro(professor.getIntro())
+                        .image(professor.getImage())
+                        .build()))
+                        .collect(Collectors.toList()))
+                .build();
     }
 }
