1c93ab036d1b3bf54a2843356c17336d20541259
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2023-10-08 14:03:29 +0900

    Fix: Repository 수정

--- src/main/java/com/sejong/creativesemester/professor/repository/ProfessorRepository.java
+++ src/main/java/com/sejong/creativesemester/professor/repository/ProfessorRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface ProfessorRepository extends JpaRepository<ProfessorRepository, Long> {
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 
     @Query("select f from PROFESSOR_TABLE f join f.major as m where m.id=:majorId order by f.name")
     Page<Professor> findAllByOrderByName(@Param(value = "majorId") Long majorId, Pageable pageable);
