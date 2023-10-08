6a9198088ab0ad1f54497a499ca629f359a076e2
jaepyo99 <jaepyo99@naver.com>
2023-10-08 18:15:26 +0900

    [Chore] 잘못 들어간 어노테이션 수정

--- src/main/java/com/sejong/creativesemester/vote/service/res/VoteDetail.java
+++ src/main/java/com/sejong/creativesemester/vote/service/res/VoteDetail.java
@@ -1,5 +1,6 @@
 package com.sejong.creativesemester.vote.service.res;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,5 +18,6 @@ public class VoteDetail {
     @Schema(description = "반대 개수")
     private Integer opposeCnt;
     @Schema(description = "마감 기한")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime deadLine;
 }
