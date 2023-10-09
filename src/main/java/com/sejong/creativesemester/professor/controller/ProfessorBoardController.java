e998ea55215e4d68b99353ba98569cc5671bf452
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2023-10-09 18:50:17 +0900

    Feat: 교수 게시판 기능을 담당하는 controller와 service 구현

--- src/main/java/com/sejong/creativesemester/professor/controller/ProfessorBoardController.java
+++ src/main/java/com/sejong/creativesemester/professor/controller/ProfessorBoardController.java
@@ -1,14 +1,19 @@
 package com.sejong.creativesemester.professor.controller;
 
+import com.sejong.creativesemester.comment.controller.req.AddCommentRequest;
 import com.sejong.creativesemester.common.format.success.SuccessResponse;
+import com.sejong.creativesemester.professor.dto.CourseListResponseDto;
+import com.sejong.creativesemester.professor.dto.EvaluationListResponseDto;
+import com.sejong.creativesemester.professor.dto.ProfessorListResponseDto;
 import com.sejong.creativesemester.professor.service.ProfessorBoardService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @RestController
@@ -19,10 +24,43 @@ public class ProfessorBoardController {
     private final ProfessorBoardService professorBoardService;
 
     // 교수 조회, 강의 게시판 조회, 학생들 평가 생성, 조회, 삭제, 신고(?)
+
+    @ApiOperation(value = "교수게시판 조회",
+    notes = "현재 학과에 해당하는 교수님 목록을 조회합니다.")
     @GetMapping()
-    public SuccessResponse getProfessorBoards(@ApiIgnore Principal principal,
-                                              @RequestParam(required = false, defaultValue = "0", value = "page") int page){
-        return new SuccessResponse(professorBoardService.getBoards(principal.getName(), page));
+    public SuccessResponse<ProfessorListResponseDto> getProfessorBoards(@ApiIgnore Authentication authentication,
+                                                                        @RequestParam(required = false, defaultValue = "0", value = "page") int page){
+        return new SuccessResponse(professorBoardService.getBoards(authentication.getName(), page));
+    }
+
+    @ApiOperation(value = "강의게시판 조회",
+    notes = "해당 교수에 해당하는 정보와 강의 목록을 조회합니다.")
+    @GetMapping("/{professorId}")
+    public SuccessResponse<CourseListResponseDto> getCourseBoards(@ApiIgnore Authentication authentication,
+                                                                  @PathVariable(value = "professorId", required = true) Long professorId,
+                                                                  @RequestParam(required = false, defaultValue = "0", value = "page") int page){
+        return new SuccessResponse(professorBoardService.getCourseBoards(professorId, authentication.getName(), page));
+    }
+
+    @ApiOperation(value = "평가댓글 생성 api",
+            notes = "강의에 대한 평가댓글을 작성합니다.")
+    @PostMapping("/{professorId}/{courseId}")
+    public SuccessResponse addEvaluation(@ApiIgnore Principal principal,
+            @PathVariable(value = "professorId", required = true) Long professorId,
+            @PathVariable(value = "courseId", required = true) Long courseId,
+            @Valid @RequestBody AddCommentRequest addCommentRequest) {
+        professorBoardService.addEvaluation(professorId, courseId, principal.getName(), addCommentRequest.toRequestDto());
+        return SuccessResponse.ok("댓글이 작성되었습니다.");
+    }
+
+    @ApiOperation(value = "평가게시글 조회 api",
+    notes = "강의에 대한 평가댓글들을 조회합니다.")
+    @GetMapping("/{professorId}/{courseId}")
+    public SuccessResponse<EvaluationListResponseDto> getEvaluationBoards(@ApiIgnore Authentication authentication,
+                                                                          @PathVariable(value = "professorId", required = true) Long professorId,
+                                                                          @PathVariable(value = "courseId", required = true) Long courseId,
+                                                                          @RequestParam(required = false, defaultValue = "0", value = "page") int page){
+        return new SuccessResponse<>(professorBoardService.getEvaluationBoards(professorId, courseId, authentication.getName(), page));
     }
 
 }
--- src/main/java/com/sejong/creativesemester/professor/service/ProfessorBoardService.java
+++ src/main/java/com/sejong/creativesemester/professor/service/ProfessorBoardService.java
@@ -1,9 +1,18 @@
 package com.sejong.creativesemester.professor.service;
 
+import com.sejong.creativesemester.board.entity.Board;
+import com.sejong.creativesemester.comment.entity.Comment;
+import com.sejong.creativesemester.comment.service.req.AddCommentRequestDto;
+import com.sejong.creativesemester.common.format.exception.board.NotFoundBoardException;
+import com.sejong.creativesemester.common.format.exception.professor.NotFoundCourseException;
+import com.sejong.creativesemester.common.format.exception.professor.NotMatchProfessorException;
 import com.sejong.creativesemester.common.format.exception.user.NotFoundUserException;
-import com.sejong.creativesemester.professor.dto.ProfessorListResponseDto;
-import com.sejong.creativesemester.professor.dto.ProfessorSimpleResponseDto;
+import com.sejong.creativesemester.professor.dto.*;
+import com.sejong.creativesemester.professor.entity.Course;
+import com.sejong.creativesemester.professor.entity.Evaluation;
 import com.sejong.creativesemester.professor.entity.Professor;
+import com.sejong.creativesemester.professor.repository.CourseRepository;
+import com.sejong.creativesemester.professor.repository.EvaluationRepository;
 import com.sejong.creativesemester.professor.repository.ProfessorRepository;
 import com.sejong.creativesemester.user.entity.User;
 import com.sejong.creativesemester.user.repository.UserRepository;
@@ -11,7 +20,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
@@ -23,6 +31,8 @@ public class ProfessorBoardService {
 
     private final UserRepository userRepository;
     private final ProfessorRepository professorRepository;
+    private final CourseRepository courseRepository;
+    private final EvaluationRepository evaluationRepository;
     private final int TOTAL_ITEMS_PER_PAGE = 20;
 
     public ProfessorListResponseDto getBoards(String studentNum, int page){
@@ -32,7 +42,7 @@ public class ProfessorBoardService {
         return ProfessorListResponseDto.builder()
                 .totalPage(list.getTotalPages())
                 .currentPage(list.getNumber())
-                .list(list.getContent().stream().map((professor -> ProfessorSimpleResponseDto.builder()
+                .list(list.getContent().stream().map((professor -> ProfessorListSimpleResponseDto.builder()
                         .boardId(professor.getId())
                         .name(professor.getName())
                         .intro(professor.getIntro())
@@ -40,5 +50,73 @@ public class ProfessorBoardService {
                         .build()))
                         .collect(Collectors.toList()))
                 .build();
+    }
+
+    public CourseListResponseDto getCourseBoards(Long professorId, String studentNum,  int page){
+        User user = userRepository.findByStudentNum(studentNum).orElseThrow(NotFoundUserException::new);
+
+        Page<Course> coursePage = courseRepository.findAllByOrderByName(professorId,
+                PageRequest.of(page, TOTAL_ITEMS_PER_PAGE));
+
+        Professor professor = professorRepository.findById(professorId).orElseThrow(NotMatchProfessorException::new);
+
+        return CourseListResponseDto.builder()
+                .totalPage(coursePage.getTotalPages())
+                .currentPage(coursePage.getNumber())
+                .list(coursePage.getContent().stream().map((course) -> CourseSimpleResponseDto.builder()
+                        .title(course.getTitle())
+                        .classification(course.getClassification())
+                        .grade(course.getGrade())
+                        .score(course.getScore())
+                        .build()).collect(Collectors.toList())
+                )
+                .professorSimpleResponseDto(ProfessorSimpleResponseDto.builder()
+                        .name(professor.getName())
+                        .image(professor.getImage())
+                        .location(professor.getLocation())
+                        .phoneNum(professor.getPhonenum())
+                        .email(professor.getEmail())
+                        .majorSub(professor.getMajorSub())
+                        .lab(professor.getLab())
+                        .build())
+                .build();
+    }
+
+    public void addEvaluation(Long professorId, Long courseId, String studentNum, AddCommentRequestDto addCommentRequestDto) {
+
+        User userByStudentNum = userRepository.findByStudentNum(studentNum).orElseThrow(NotFoundUserException::new);
+        Course course = courseRepository.findById(courseId).orElseThrow(NotFoundCourseException::new);
+
+        if(!(course.getProfessor().getId()).equals(professorId)){
+            throw new NotMatchProfessorException();
+        }
+
+        Evaluation evaluation = evaluationRepository.save(Evaluation.builder()
+                .text(addCommentRequestDto.getText())
+                .course(course)
+                .user(userByStudentNum)
+                .build());
+
+        log.info("text: {}", evaluation.getText());
+        evaluation.getUser().addEvaluation(evaluation);
+    }
+
+    public EvaluationListResponseDto getEvaluationBoards(Long professorId, Long courseId, String studentNum, int page){
+
+        Course course = courseRepository.findById(courseId).orElseThrow(NotFoundCourseException::new);
+        if(!(course.getProfessor().getId()).equals(professorId)){
+            throw new NotMatchProfessorException();
+        }
+        Page<Evaluation> evaluationPage = evaluationRepository.findAllByCreatedTime(courseId, PageRequest.of(page, TOTAL_ITEMS_PER_PAGE));
+
+        return EvaluationListResponseDto.builder()
+                .totalPage(evaluationPage.getTotalPages())
+                .currentPage(evaluationPage.getNumber())
+                .evaluationList(evaluationPage.getContent().stream().map((evaluation) -> EvaluationSimpleResponseDto.builder()
+                        .name(evaluation.getUser().getName())
+                        .text(evaluation.getText())
+                        .createdTime(evaluation.getCreatedTime())
+                        .build())
+                        .collect(Collectors.toList())).build();
     }
 }
