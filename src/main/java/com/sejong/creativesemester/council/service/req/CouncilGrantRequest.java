86e00e556310a86c782c7b2672273c7874b17e1a
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2023-11-07 17:29:48 +0900

    [Fix] HTTP Method 변경

--- src/main/java/com/sejong/creativesemester/council/service/req/CouncilGrantRequest.java
+++ src/main/java/com/sejong/creativesemester/council/service/req/CouncilGrantRequest.java
@@ -1,14 +1,11 @@
 package com.sejong.creativesemester.council.service.req;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@Builder
 public class CouncilGrantRequest {
 
     private String grantCode;
