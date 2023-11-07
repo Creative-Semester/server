44742945bd5eb93794a261c48de4fcfb11333850
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2023-11-07 20:19:39 +0900

    [Refactor] Builder 위치 변경

--- src/main/java/com/sejong/creativesemester/council/entity/GrantCode.java
+++ src/main/java/com/sejong/creativesemester/council/entity/GrantCode.java
@@ -3,6 +3,7 @@ package com.sejong.creativesemester.council.entity;
 import com.sejong.creativesemester.common.domain.BaseTimeEntity;
 import com.sejong.creativesemester.major.entity.Major;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Builder
 @Entity(name = "GRANTCODE_TABLE")
 public class GrantCode extends BaseTimeEntity {
 
