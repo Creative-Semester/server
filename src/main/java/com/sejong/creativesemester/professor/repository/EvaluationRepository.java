f29c60dc654689bfbc5ff32b1219388856e96243
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2023-11-15 00:58:26 +0900

    [Refactor] 어노테이션 추가

--- src/main/java/com/sejong/creativesemester/professor/repository/EvaluationRepository.java
+++ src/main/java/com/sejong/creativesemester/professor/repository/EvaluationRepository.java
@@ -7,10 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
 
     @Query("select e from EVALUATION_TABLE e join e.course as c where c.id=:courseId order by e.createdTime")
     Page<Evaluation> findAllByCreatedTime(@Param(value = "courseId") Long courseId, Pageable pageable);
+
 }
